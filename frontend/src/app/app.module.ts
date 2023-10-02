8065206787aa2893f9bcc42a40d980dcef1a3352
kumarnivi <nivethikashivakumar56@gmail.com>
2023-10-02 14:58:27 +0530

    sidebar and leave-allocation are finished

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { LeaveAllocationComponent } from './components/leave-allocation/leave-allocation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    LeaveAllocationComponent
   ],
   imports: [
     BrowserModule,
