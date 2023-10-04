3a1c456a0781d9189a2a60a6e1532ea54e58a809
kumarnivi <nivethikashivakumar56@gmail.com>
2023-10-04 23:05:01 +0530

    modified the calender

--- frontend/src/app/admin/graph/graph.component.ts
+++ frontend/src/app/admin/graph/graph.component.ts
@@ -63,73 +63,6 @@ export class GraphComponent {
 	}	
   
 
-// bar chart
-// chartOptions1 = {
-//   animationEnabled: true,
-//   title: {
-//   text: "Leave type brakedown"
-//   },
-//   axisX: {
-//   labelAngle: -90
-//   },
-//   axisY: {
-//   title: "billion of barrels"
-//   },
-//   axisY2: {
-//   title: "million barrels/day"
-//   },
-//   toolTip: {
-//   shared: true
-//   },
-//   legend:{
-//   cursor:"pointer",
-//   itemclick: function(e: any){
-//     if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
-//     e.dataSeries.visible = false;
-//     }
-//     else {
-//     e.dataSeries.visible = true;
-//     }
-//     e.chart.render();
-//   }
-//   },
-//   data: [{
-//   type: "column",	
-//   name: "Proven Oil Reserves (bn)",
-//   legendText: "Proven Oil Reserves",
-//   showInLegend: true, 
-//   dataPoints:[
-//     {label: "Saudi", y: 262},
-//     {label: "Venezuela", y: 211},
-//     {label: "Canada", y: 175},
-//     {label: "Iran", y: 137},
-//     {label: "Iraq", y: 115},
-//     {label: "Kuwait", y: 104},
-//     {label: "UAE", y: 97.8},
-//     {label: "Russia", y: 60},
-//     {label: "US", y: 23.3},
-//     {label: "China", y: 20.4}
-//   ]
-//   }, {
-//   type: "column",	
-//   name: "Oil Production (million/day)",
-//   legendText: "Oil Production",
-//   axisYType: "secondary",
-//   showInLegend: true,
-//   dataPoints:[
-//     {label: "Saudi", y: 11.15},
-//     {label: "Venezuela", y: 2.5},
-//     {label: "Canada", y: 3.6},
-//     {label: "Iran", y: 4.2},
-//     {label: "Iraq", y: 2.6},
-//     {label: "Kuwait", y: 2.7},
-//     {label: "UAE", y: 3.1},
-//     {label: "Russia", y: 10.23},
-//     {label: "US", y: 10.3},
-//     {label: "China", y: 4.3}
-//   ]
-//   }]
-// }
 
 
 chartOptions1 = {
@@ -137,10 +70,10 @@ chartOptions1 = {
     text: "Leave type brakedown"
   },
   axisX: {
-    title: "Month", // X-axis label
-    interval: 1, // Interval between tick marks (set to 1 to show all months)
-    intervalType: "month", // Specify the interval type as "month"
-    valueFormatString: "MMM" // Format for displaying month names (e.g., Jan, Feb, Mar)
+    title: "Month",
+    interval: 1,
+    intervalType: "month", 
+    valueFormatString: "MMM" 
   },
   data: [
     {
